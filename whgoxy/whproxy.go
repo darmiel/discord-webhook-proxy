1a9f3365bfca7c03b113869cb0bd8e007d8ae8e1
darmiel <71837281+darmiel@users.noreply.github.com>
2021-01-03 23:38:14 +0100

    Removed unused vars

--- whgoxy/whproxy.go
+++ whgoxy/whproxy.go
@@ -1,40 +1,27 @@
 package whgoxy
 
 import (
-	"fmt"
 	"github.com/darmiel/whgoxy/db"
-	"github.com/darmiel/whgoxy/discord"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
-var Opt *Options
-var WHDatabase db.Database
-
 func New(options *Options) {
-	Opt = options
+	var database db.Database
+	var err error
 
 	if options.MongoUse {
 		log.Println("👉 Using mongo as database!")
 
 		uri := db.BuildApplyURI(options.MongoAuthUser, options.MongoAuthPass, options.MongoHost, options.MongoDatabase)
-		database, err := db.ConnectMongoDatabase(uri, options.MongoDatabase)
+		database, err = db.ConnectMongoDatabase(uri, options.MongoDatabase)
 		if err != nil {
 			log.Fatalln("Fatal: ", err)
 			return
 		}
-		log.Println("✅ All done!")
-
-		webhook := discord.NewWebhook("https://ptb.discord.com/api/webhooks/794739759909830666/6wjgrcP8emYcE4-awAd8G83aiF8gkaOEF84kiSOfg8Giaife4kF", &discord.WebhookData{"content": "jo"})
-		if err := webhook.CheckValidity(true); err != nil {
-			fmt.Println("Warn: Created webhook is not valid:", err)
-		}
-
-		log.Println("Okidoki")
-
-		WHDatabase = database
+		log.Println("✅  All done!")
 	} else {
 		// No database selected
 		log.Fatalln("Error: No database selected.")
@@ -48,7 +35,7 @@ func New(options *Options) {
 
 	defer func() {
 		log.Println("[Shutdown] Closing database connection")
-		if err := WHDatabase.Disconnect(); err != nil {
+		if err := database.Disconnect(); err != nil {
 			log.Fatalln("Fatal:", err)
 		}
 	}()
