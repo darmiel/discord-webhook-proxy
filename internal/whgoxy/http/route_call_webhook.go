493f175485768854cfd5ccfed1b50707685117ce
darmiel <71837281+darmiel@users.noreply.github.com>
2021-02-01 21:55:04 +0100

    Implement show url

--- internal/whgoxy/http/route_call_webhook.go
+++ internal/whgoxy/http/route_call_webhook.go
@@ -7,55 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
 )
-
-func (ws *WebServer) safeWebhookRouteHandler(w http.ResponseWriter, r *http.Request) {
-	// parse vars
-	vars := mux.Vars(r)
-	userID := vars["user_id"]
-	uid := vars["uid"]
-	secret := vars["secret"]
-
-	log.Println("Requested webhook", uid, "by", userID, "with secret", secret)
-
-	// get database connection
-	db := ws.Database
-
-	// try to get webhook with uid
-	webhook, err := db.FindWebhook(uid, "abc")
-	if err != nil {
-		_, _ = fmt.Fprintf(w, "Error (Database): %s", err.Error())
-		return
-	}
-
-	// check secret
-	if webhook.Secret != secret {
-		_, _ = fmt.Fprintf(w, "Error (Auth): Secret mismatch")
-		return
-	}
-
-	// build params
-	params := make(map[string]string)
-	query := r.URL.Query()
-	for k, v := range query {
-		if strings.HasSuffix(k, "[]") {
-			k = k[:len(k)-2]
-		}
-
-		params[k] = strings.Join(v, ",")
-		log.Println("[Debug] Param", k, "=", params[k])
-	}
-
-	// send webhook
-	sentJson, err := webhook.Send(params)
-	if err != nil {
-		_, _ = fmt.Fprintf(w, "Error (Discord): %s", err.Error())
-		return
-	}
-
-	_, _ = fmt.Fprintf(w, "Success: %s", sentJson)
-}
 
 // /call/json/{userid}/{uid}/{secret}
 func (ws *WebServer) jsonWebhookRouteHandler(w http.ResponseWriter, r *http.Request) {
