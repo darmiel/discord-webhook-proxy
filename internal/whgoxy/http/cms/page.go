88732fd72f17092091a0d08db1c7700341a4a4bd
darmiel <71837281+darmiel@users.noreply.github.com>
2021-02-10 15:19:01 +0100

    Added proper bson field names

--- internal/whgoxy/http/cms/page.go
+++ internal/whgoxy/http/cms/page.go
@@ -6,28 +6,28 @@ import (
 )
 
 type CMSPageMeta struct {
-	Title         string
-	CreatorUserID string
-	CreatedAt     time.Time
+	Title         string    `bson:"title" json:"title"`
+	CreatorUserID string    `bson:"creator_user_id" json:"creator_user_id"`
+	CreatedAt     time.Time `bson:"created_at" json:"created_at"`
 }
 
 type CMSPagePreferences struct {
-	AuthorVisible    bool
-	Dynamic          bool // Does the page include GoLang-Template content?
-	URLCaseSensitive bool
+	AuthorVisible    bool `bson:"author_visible" json:"author_visible"`
+	Dynamic          bool `bson:"dynamic" json:"dynamic"` // Does the page include GoLang-Template content?
+	URLCaseSensitive bool `bson:"url_case_sensitive" json:"url_case_sensitive"`
 }
 
 type CMSPageUpdate struct {
-	UpdatedAt     time.Time // Time of update
-	UpdaterUserID string    // UserID of updater, -1 = System
+	UpdatedAt     time.Time `bson:"updated_at" json:"updated_at"`           // Time of update
+	UpdaterUserID string    `bson:"updater_user_id" json:"updater_user_id"` // UserID of updater, -1 = System
 }
 
 type CMSPage struct {
-	URL         string
-	Meta        CMSPageMeta
-	Updates     []CMSPageUpdate
-	Preferences CMSPagePreferences
-	Content     string
+	URL         string             `bson:"url" json:"url"`
+	Meta        CMSPageMeta        `bson:"meta" json:"meta"`
+	Updates     []CMSPageUpdate    `bson:"updates" json:"updates"`
+	Preferences CMSPagePreferences `bson:"preferences" json:"preferences"`
+	Content     string             `bson:"content" json:"content"`
 }
 
 func (p *CMSPage) CreateFilter() *bson.M {
